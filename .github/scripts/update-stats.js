01fd89a516e822a416e1dbebeef5d3d7dded6085
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-12-08 10:11:14 +0100

    chore: update readme

--- .github/scripts/update-stats.js
+++ .github/scripts/update-stats.js
@@ -300,6 +300,8 @@ async function updateReadme() {
 
 **Languages**
 
+| | |
+|---|---|
 ${languageStats.map((stat) => {
   const encodedLang = encodeURIComponent(stat.lang);
   const searchUrl = `https://github.com/${USERNAME}?tab=repositories&q=language:${encodedLang}`;
@@ -348,11 +350,11 @@ ${languageStats.map((stat) => {
     ? `https://img.shields.io/badge/${encodedLang}-${stat.percentage}%25-${colorCode}?style=flat&logo=${logo}&logoColor=white`
     : `https://img.shields.io/badge/${encodedLang}-${stat.percentage}%25-${colorCode}?style=flat`;
   
-  const barLength = Math.round(parseFloat(stat.percentage) / 2.5);  // Changed from /4 to /2.5 for slightly bigger
-  const bar = '█'.repeat(barLength) + '░'.repeat(40 - barLength);  // Changed from 25 to 40
+  const barLength = Math.round(parseFloat(stat.percentage) / 2.5);
+  const bar = '█'.repeat(barLength) + '░'.repeat(40 - barLength);
     
-  return `[![${stat.lang}](${badgeUrl})](${searchUrl})  \n\`${bar}\``;  // Wrapped in backticks for monospace
-}).join('\n\n')}
+  return `| [![${stat.lang}](${badgeUrl})](${searchUrl}) | \`${bar}\` |`;
+}).join('\n')}
 
 </td>
 </tr>
